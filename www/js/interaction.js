34a7bfae14cf161b2b58a5651cefb78afdffd27a
zhenyi.zhang <zhenyi.zhang@viziner.cn>
2016-01-21 13:44:30 +0800

    update create/update issue

--- www/js/interaction.js
+++ www/js/interaction.js
@@ -7,21 +7,23 @@ function tipmessage(message,id){
  	setTimeout(function(){$("#"+id).animate({opacity:0},300);setTimeout(function(){$("#"+id).remove();$("#msgContainer").hide();},100);},2000); 
 }
 //提示信息带图片
-function tipmessage1(message,img,id){
-	$("#loadingContainer").append("<div class='tipmessage' id='"+id+"'><span>"+img+message+"</span></div>");
+function tipmessage1(message,id){
+	$("#loadingContainer").append("<div class='tipmessage' id='"+id+"'><span><img src='img/loading.gif'><br/>"+message+"</span></div>");
 	$("#loadingContainer").show();
 	$("#"+id).animate({opacity:1},300);
 }
 //改变提示内容，需要首先使用tipmessage1，结束时调用closetipmessage1
 function changeTipmessage(message,id){
 	$("#"+id).remove();
-	$("#loadingContainer").append("<div class='tipmessage' id='"+id+"'><span>"+message+"</span></div>");
-//	$("#loadingContainer").show();
-//	$("#"+id).animate({opacity:1},300);
+	$("#loadingContainer").append("<div class='tipmessage' id='"+id+"'><span><img src='img/loading.gif'><br/>"+message+"</span></div>");
+	$("#loadingContainer").show();
+	$("#"+id).animate({opacity:1},300);
 }
 function closetipmessage1(id){
 	setTimeout(function(){$("#"+id).animate({opacity:0},300);setTimeout(function(){$("#"+id).remove();
-	$("#loadingContainer").hide();
+	if($("#loadingContainer").childNodes.length <= 0){
+		$("#loadingContainer").hide();
+	}
 	},100);},2000);
 	
 }
